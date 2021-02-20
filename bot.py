20e7286f8cf140b09b2f89eefca01586c0b7f4d7
TheNitG <nitish.gorentala@gmail.com>
2021-02-20 17:20:39 -0500

    Added more context in the embeds.

--- bot.py
+++ bot.py
@@ -20,15 +20,16 @@ async def build_embed(message, senator):
     embed.set_author(name='SenateStockWatch Bot')
 
     embed.add_field(name='Senator Name', value=f'{senator["first_name"]} {senator["last_name"]}', inline=False)
-    embed.add_field(name='History of Transactions', value='#'*20, inline=False)
+    embed.add_field(name='Transactions Reported Today', value='#'*20, inline=False)
 
     transactions = senator['transactions']
     for num, transaction in enumerate(transactions):
         print(transaction)
         ticker = transaction['ticker']
         ticker = 'N/A' if ticker == '--' else ticker[ticker.index('>') + 1:ticker.index('</')]
-        embed.add_field(name=f'Transaction {num + 1}: ({transaction["type"]})', value=f'Ticker: {ticker} | '
-        f'{re.sub(r" <.*", "", transaction["asset_description"])}\nAmount: {transaction["amount"]}', inline=False)
+        embed.add_field(name=f'Transaction {num + 1}: ({transaction["type"]})', value=f'Transaction Date: '
+        f'{transaction["transaction_date"]}\nTicker: {ticker} | {re.sub(r" <.*", "", transaction["asset_description"])}'
+        f'\nAmount: {transaction["amount"]}', inline=False)
     embed.set_footer(text='Thanks for using SenateStockWatch Bot!')
 
     await message.channel.send(embed=embed)
@@ -61,25 +62,25 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.idle, activity=discord.Game("Listening to s!help"))
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game("Listening to !help"))
     print(f'{client.user.name} has connected to Discord!')
 
 
 @client.event
 async def on_message(message):
     content = message.content
-    if re.search(r'^s!today$', content):
+    if re.search(r'^!today$', content):
         try:
             await obtain_data(message)
         except:
             await message.channel.send('No data for today as of right now')
-    elif re.search(r'^s!day \d{1,2}/\d{1,2}/\d{4}$', content):
+    elif re.search(r'^!day \d{1,2}/\d{1,2}/\d{4}$', content):
         try:
-            day_parts = content[6:].split('/')
+            day_parts = content[5:].split('/')
             day = f'{int(day_parts[0]):02d}_{int(day_parts[1]):02d}_{int(day_parts[2])}'
             await obtain_data(message, day)
         except:
-            await message.channel.send(f'No data for {content[6:]} as of right now')
+            await message.channel.send(f'No data for {content[5:]} as of right now')
 
 
 client.run(TOKEN)
